1984e5c1ccd1e71dd6a21f59656c58b1190aaeb7
DuyLeVu <levuduy132@gmail.com>
2021-09-23 09:43:05 +0700

    Sua bai 23/09

--- UngDungQuanLy/script.js
+++ UngDungQuanLy/script.js
@@ -1,55 +1,54 @@
-let testArr = ["Sony Xperia", "Samsung Galaxy", "Nokia 6"];
-productDisplay(testArr);
+let arrProduct = ["Sony Xperia", "Samsung Galaxy", "Nokia 6"];
+displayProduct(arrProduct);
 
-function productAdd() {
-    let productValue = document.getElementById('productInput').value;
-    testArr.push(productValue);
-    productDisplay(testArr);
+function addProduct() {
+    let valueProduct = document.getElementById('productInput').value;
+    arrProduct.push(valueProduct);
+    displayProduct(arrProduct);
 }
 
-function productDisplay(newArr) {
+function displayProduct(newArr) {
     let data = "<table>";
     for (let i = 0; i < newArr.length; i++) {
         data = data + "<tr>" + "<td class='product'>" + newArr[i] + "</td>";
-        data = data + "<td><button class='btn' onclick='productEdit(testArr)'>Edit</button></td>"
-            + "<td><button class='btn' id='btndelete' onclick='productDelete(" + i + ")'>Delete</button></td>";
-
+        data = data + "<td><button class='btn' onclick='editProduct(arrProduct)'>Edit</button></td>"
+            + "<td><button class='btn' id='btndelete' onclick='deleteProduct(" + i + ")'>Delete</button></td>";
         data = data + "</tr>";
     }
     data = data + "</table>";
     document.getElementById("output").innerHTML = data;
 }
 
-function productEdit(newArr) {
+function editProduct(newArr) {
     let data2 = "<select name='' id='optionEdit'><option>All product</option>";
     for (let i = 0; i < newArr.length; i++) {
         data2 = data2 + "<option value=" + i + ">" + newArr[i] + "</option>";
     }
     data2 = data2 + "</select>";
     data2 = data2 + "<input class='edit_input' type='text' id='inputEdit'>"
-    data2 = data2 + "<button class='btn' type='button' onclick='productChangeName()'>Change Name</button>";
+    data2 = data2 + "<button class='btn' type='button' onclick='changeProductName()'>Change Name</button>";
     console.log(newArr);
     document.getElementById("outputEdit").innerHTML = data2;
 
 }
 
-function productChangeName() {
+function changeProductName() {
     let productTxtEdit = document.getElementById('inputEdit').value;
     // console.log(productTxtEdit);
     let index = document.getElementById('optionEdit').value;
     // console.log(index);
     // let index = e.options(e.selectedIndex);
     // console.log(index);
-    for (let i = 0; i < testArr.length; i++) {
+    for (let i = 0; i < arrProduct.length; i++) {
         if (i == index) {
-            testArr[i] = productTxtEdit;
+            arrProduct[i] = productTxtEdit;
         }
     }
-    productDisplay(testArr);
+    displayProduct(arrProduct);
 }
 
-function productDelete(index) {
+function deleteProduct(index) {
     // console.log(n);
-    testArr.splice(index, 1);
-    productDisplay(testArr);
+    arrProduct.splice(index, 1);
+    displayProduct(testArr);
 }
